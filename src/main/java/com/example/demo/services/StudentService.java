be763c0c5af52afebf3ffa190a3a379c01960052
mahesh.bansode+1-at-603658633613 <mahesh.b@livianorooms.com>
2021-01-27 17:17:32 +0530

    Added the jpa repository

--- src/main/java/com/example/demo/services/StudentService.java
+++ src/main/java/com/example/demo/services/StudentService.java
@@ -1,22 +1,34 @@
 package com.example.demo.services;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.example.demo.models.Student;
+import com.example.demo.repositories.StudentRepository;
 
 @Service
 public class StudentService {
+	
+	private final StudentRepository studentRepository;
+	
+	@Autowired
+	public StudentService(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
 
 	public List<Student> getStudents() {
-		List<Student> students = new ArrayList<Student>();
-
-		// Generating student list
-		for (int i = 0; i < 15; i++) {
-			students.add(new Student(i, "Student : " + i, i + 35));
-		}
-
-		return students;
+		return this.studentRepository.findAll();
+	}
+	
+	public Optional<Student> getStudent(Integer studentId) {
+		return this.studentRepository.findById(studentId);
+	}
+	
+	public void addStudent(Student student) {
+		this.studentRepository.save(student);
 	}
 
 }
